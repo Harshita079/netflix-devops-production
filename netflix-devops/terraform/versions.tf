30177335d2ab229b6cfe213985146842357ed94b
Harshita Pawar <harshu.shreyash64@gmail.com>
2026-05-09 22:47:13 +0530

    fixed backend duplicate

--- netflix-devops/terraform/versions.tf
+++ netflix-devops/terraform/versions.tf
@@ -7,10 +7,4 @@ terraform {
       version = "~> 5.0"
     }
   }
-
-  backend "s3" {
-    bucket = "harshita-netflix-tfstate-2026"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
-  }
 }
