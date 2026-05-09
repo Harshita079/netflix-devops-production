26fcb4f816bd886c1db0fcc9eeca10881afc5553
Harshita Pawar <harshu.shreyash64@gmail.com>
2026-05-09 19:41:51 +0530

    final aws creds fix

--- netflix-devops/terraform/terraform.tfvars
+++ netflix-devops/terraform/terraform.tfvars
@@ -1,3 +1,4 @@
-# terraform.tfvars
-
-region = "us-east-1"
+aws_region   = "us-east-1"
+instance_type = "t2.micro"
+ami_id       = "ami-0c02fb55956c7d316"
+key_name     = "netflix-key"
--- netflix-devops/terraform/versions.tf
+++ netflix-devops/terraform/versions.tf
@@ -1,18 +1,10 @@
-# versions.tf
-
 terraform {
-  required_version = ">= 1.6.0"
+  required_version = ">= 1.5.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
-  }
-
-  backend "s3" {
-    bucket = "harshita-netflix-tfstate-2026"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
   }
 }
