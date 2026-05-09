f2228d932217d580c099ca5695c94365e2c0f3b3
Harshita Pawar <harshu.shreyash64@gmail.com>
2026-05-09 16:26:35 +0530

    fixed pipeline checkout final change

--- netflix-devops/terraform/backend.tf
+++ netflix-devops/terraform/backend.tf
@@ -2,7 +2,7 @@
 
 terraform {
   backend "s3" {
-    bucket = "harshita-netflix-tfstate-2026 "
+    bucket = "harshita-netflix-tfstate-2026"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
